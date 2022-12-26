5949cb8c1b53252b31497426ccfd2d4457a2d4db
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-12-26 06:38:25 +0100

    removed request

--- Commands/MatchmakingCommands.cs
+++ Commands/MatchmakingCommands.cs
@@ -5,18 +5,6 @@ using DSharpPlus.Entities;
 using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.Interactivity.EventHandling;
 using DSharpPlus.Interactivity.Enums;   
-using DSharpPlus.EventArgs;
-using DSharpPlus.Interactivity;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
-using System.Linq;
 
 
 
