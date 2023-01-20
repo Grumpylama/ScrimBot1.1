13914f62cfe79c363923f8d5eb74ae745951a219
Krenar Manxhuka <manxhuka01@hotmail.com>
2023-01-20 19:20:43 +0100

    findMatch()
    Added almost all of it except the sending DMs to team captains.

--- Classes/Matchmaking/MatchMaker.cs
+++ Classes/Matchmaking/MatchMaker.cs
@@ -23,7 +23,74 @@ namespace big
       //the bot continues to the next index on list.
       public void findMatch()
       {
-         
+         // make sure matchmakingTeams have at least 2 teams
+        if (MMTList.Count < 2)
+        {
+            return false;
+        }
+
+        // create a dictionary for storing teams that have declined matches
+        Dictionary<string, int> declinedTeams = new Dictionary<string, int>();
+
+        while (MMTList.Count > 1)
+        {
+            // select the first team
+            Team team1 = MMTList[0];
+
+            // set the initial difference to the maximum value
+            int minDifference = int.MaxValue;
+            Team team2 = null;
+
+            // compare the team to all other teams
+            for (int i = 1; i < MMTList.Count; i++)
+            {
+                Team currentTeam = MMTList[i];
+
+                // calculate the difference in MMR
+                int difference = Math.Abs(team1.MMR - currentTeam.MMR);
+
+                // check if the difference is the smallest so far
+                if (difference < minDifference)
+                {
+                    minDifference = difference;
+                    team2 = currentTeam;
+                }
+            }
+
+            // check if the team captain of team1 has declined a match before
+            if (declinedTeams.ContainsKey(team1.teamID))
+            {
+                // check if the team captain of team1 has declined 3 or more matches
+                if (declinedTeams[team1.teamID] >= 3)
+                {
+                    // move the team to a lower priority list
+                    MMTList.Remove(team1);
+                    continue;
+                }
+            }
+
+            // check if the team captain of team2 has declined a match before
+            if (declinedTeams.ContainsKey(team2.teamID))
+            {
+                // check if the team captain of team2 has declined 3 or more matches
+                if (declinedTeams[team2.teamID] >= 3)
+                {
+                    // move the team to a lower priority list
+                    MMTList.Remove(team2);
+                    continue;
+                }
+            }
+
+            // send DMs to the team captains to ask if they want to accept the match
+            //var accept = await SendMatchAcceptDM(team1, team2);
+            //if (!accept) {
+            //    continue;
+            //}
+            // if the team captain accept the match, remove the team from matchmaking list
+            MMTList.Remove(team1);
+            MMTList.Remove(team2);
+        }
+        return true;
       }
 
       //Time threshhold nu time minus när man ska spela time (2h för att inte hamna i queue)
