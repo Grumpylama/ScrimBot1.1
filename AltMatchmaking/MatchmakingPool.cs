fb18a7d94e42eca8d160c6f348e6dd8b8660a602
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2023-09-03 17:40:00 +0200

    Matchmakingloop

--- AltMatchmaking/MatchmakingPool.cs
+++ AltMatchmaking/MatchmakingPool.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
 namespace big
 {
     internal class MatchmakingPool
@@ -33,6 +36,63 @@ namespace big
 
             
 
+
+        }
+
+        public Tuple<MatchmakingTicket, MatchmakingTicket>? MatchTickets(MatchmakingTicket ticketToCheck, List<MatchmakingTicket> tickets)
+        {
+            foreach(MatchmakingTicket t in tickets)
+            {
+                if(tickets == null)
+                {
+                    StandardLogging.LogDebug(FilePath, "Tickets list is null");
+                    return null;
+                }
+
+                if(tickets.Count == 0)
+                {
+                    StandardLogging.LogDebug(FilePath, "Tickets list is empty");
+                    return null;
+                }
+
+                if (ticketToCheck is null)
+                {
+                    StandardLogging.LogDebug(FilePath, "Ticket to check is null");
+                    return null;
+                }
+
+                if (ticketToCheck.checkedTickets.Contains(t.ticketID))
+                {
+                    StandardLogging.LogDebug(FilePath, "Ticket to check has already been checked with this ticket");
+                    continue;
+                }
+
+                if (t.ticketID == ticketToCheck.ticketID)
+                {
+                    StandardLogging.LogError(FilePath, "Ticket to check is the same as the ticket to match with. Don't know how but well done");
+                    continue;
+                }
+
+                if(ticketToCheck.team == t.team)
+                {
+                    StandardLogging.LogError(FilePath, "Ticket to check has the same team as the ticket to match with");
+                    continue;
+                }
+
+
+                if(!Rules.Any(x => x.Evavluate(new MatchmakingContext(Matchtime, Tickets.Count, 0,  new List<MatchmakingTicket>(){ticketToCheck, t}))))
+                {
+                    StandardLogging.LogInfo(FilePath, "Tickets " + ticketToCheck.ticketID + " and " + t.ticketID + " are a match for the time " + Matchtime + " with the game " + game.GameName);
+                    return new Tuple<MatchmakingTicket, MatchmakingTicket>(ticketToCheck, t);
+                }
+
+                ticketToCheck.checkedTickets.Add(t.ticketID);
+
+                
+            }
+
+
+            return MatchTickets(tickets.ElementAt(0), tickets.Skip(1).ToList());
 
         }
 
